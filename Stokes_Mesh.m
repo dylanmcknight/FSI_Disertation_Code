1ad143cf2eadc44df434f7b832e9980aeba73072
dylanmcknight <143452165+dylanmcknight@users.noreply.github.com>
2024-04-29 11:09:25 -0500

    Add files via upload

---
+++ Stokes_Mesh.m
@@ -0,0 +1,39 @@
+%This program inputs a square mesh and outputs the Taylor Hood mesh
+ 
+% clear
+% close all
+
+%% Load in the square mesh
+%  Square41
+%    Square145
+%    Square545
+     Square2113
+%   Square8321
+%    Square33025
+
+%% Define the relevant mesh parameters
+
+Mf = msh.nbNod;
+Lf = length(msh.TRIANGLES6(:,1));
+Tf = length(msh.TRIANGLES6(1,:))-1;
+
+J0f = msh.LINES3(:,1:3);
+J0f = unique(J0f);
+M0f = length(J0f);
+
+Jf = transpose(setdiff(1:Mf,J0f));
+Nf = length(Jf);
+
+%% Set up the pressure mesh.
+
+Jp = unique(msh.TRIANGLES6(:,1:3)); %Index set to translate between pressure nodes and fluid nodes
+Mp = length(Jp);
+
+msh.TH3 = zeros(Lf,4);
+
+for ell = 1:1:Lf
+    for r = 1:1:3
+        msh.TH3(ell,r) = find(Jp == msh.TRIANGLES6(ell,r));
+    end
+end
+
